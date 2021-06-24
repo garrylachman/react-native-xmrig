f42fda0e3ce25ea0e1205a1922a5016c9248448b
Garry Lachman <garry@lachman.co>
2021-06-24 12:42:13 +0300

    disable opacity if not working

--- src/components/miner/screens/log.screen.tsx
+++ src/components/miner/screens/log.screen.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     hidden: {
-        opacity: 0.3
+        opacity: 1
     },
     section: {
         flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10, paddingTop: 0
