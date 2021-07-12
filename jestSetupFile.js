6450a4c527175a8c6d3eb44d13ca869543526fd4
Garry Lachman <garry@lachman.co>
2021-07-12 03:55:39 +0300

    Animated mock

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,3 +1,5 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
