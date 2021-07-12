ab441cbdf48012d65b3cd73662d9cb500107886a
Garry Lachman <garry@lachman.co>
2021-07-12 23:03:51 +0300

    testing

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
     '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
   },
   // this is specific to the Jest repo, not generally needed (the files we ignore will be in node_modules which is ignored by default)
-  transformIgnorePatterns: ['node_modules/@ui-kitten'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@ui-kitten)',
+  ],
   setupFiles: ["./jestSetupFile.js"],
   snapshotResolver: "./snapshotResolver.js"
 };
