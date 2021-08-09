928e32fbe5a77f9fda907e83aaf5304f7accec23
Garry Lachman <garry@lachman.co>
2021-08-09 03:29:59 +0300

    History charts

--- src/components/settings/screens/history.screen.tsx
+++ src/components/settings/screens/history.screen.tsx
@@ -3,6 +3,8 @@ import { NativeModules, ScrollView, StyleSheet, View } from 'react-native';
 import { Card, Divider, Layout, StyleService, Text, useStyleSheet } from '@ui-kitten/components';
 import { ColumnProps, Table } from 'react-native-awesome-table';
 import { CellContentProps } from 'react-native-awesome-table/src/components/Table';
+import { VictoryBar, VictoryChart, VictoryLine, VictoryTheme } from 'victory-native';
+import { useInterval } from '../../../core/hooks';
 
 const { XMRigModule } = NativeModules;
 
@@ -39,6 +41,12 @@ const HistoryScreen = () => {
             .catch(() => {});
     }, [])
 
+    useInterval(() => {
+        XMRigModule.getMinerHistoryBySessionAndAlgo()
+            .then((res: any) => setNativeData(res))
+            .catch(() => {});
+    }, 60*1000)
+
     const data = React.useMemo(() => {
         if (nativeData)   {
             return nativeData.map((row: any, index: number) => ({
@@ -49,12 +57,54 @@ const HistoryScreen = () => {
         return [];
     }, [nativeData]);
 
+    const hashrateChart = React.useMemo(() => {
+        if (nativeData)   {
+            return nativeData.map((row: any, index: number) => ({
+                x: new Date(row.start_date),
+                y: parseFloat(row.avg_hashrate.toFixed(1)),
+            }))
+        }
+        return [];
+    }, [nativeData]);
+
+    const miningTimeChart = React.useMemo(() => {
+        if (nativeData)   {
+            return nativeData.map((row: any, index: number) => ({
+                x: new Date(row.start_date),
+                y: parseFloat(row.mining_in_minutes),
+            }))
+        }
+        return [];
+    }, [nativeData]);
+
+    
     return (
         <Layout style={styles.layout} level='2'>
             <ScrollView>
                 <View style={styles.cards}>
                     <Text category="h6">Mining Sessions History</Text>
                 </View>
+                <Text category="h6"> Hashrate / Session</Text>
+                <VictoryChart height={200} theme={VictoryTheme.material} padding={{ top: 10, bottom: 40, left: 40, right: 40 }}>
+                <VictoryBar  
+                    data={hashrateChart} 
+                    
+                    style={{data: { fill: 'rgba(134, 65, 244)'}}}
+                    alignment="start"
+                />
+                </VictoryChart>
+
+                <Text category="h6">Minutes Mining / Session</Text>
+                <VictoryChart height={200} theme={VictoryTheme.material} padding={{ top: 10, bottom: 40, left: 40, right: 40 }}>
+                <VictoryLine  
+                    data={miningTimeChart} 
+                    
+                    style={{data: { stroke: 'rgba(134, 65, 244)'}}}
+                    
+                />
+                </VictoryChart>
+
+                <Text category="h6">History by Session/Algo</Text>
                 <Table 
                     columns={columns}
                     data={data}
@@ -81,6 +131,7 @@ const style = StyleService.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'flex-start',
+        paddingBottom: 10
     },
     row: {
         flexDirection: 'row',
